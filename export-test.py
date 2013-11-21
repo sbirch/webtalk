dae11a2e14a3ad398635b02a3fdd2eaca80b76ed
Andrew Kovacs <andrewkov@Andrews-MacBook-Air.local>
2013-11-21 11:53:59 -0500

    Added text features, organized things, added underscore

--- export-test.py
+++ export-test.py
@@ -1,31 +1,33 @@
 from selenium import webdriver, selenium
 import sys
 
-print sys.path
 import time
 
 from util.perf import tick, tock
 
 get_features_js = "js/get-features.js"
+underscore_js = "js/underscore-min.js"
 
-driver = webdriver.Firefox() #seutil.get_driver()
+driver = webdriver.phantomjs.webdriver.WebDriver() #seutil.get_driver()
+#driver = webdriver.Firefox()
 
 driver.get("http://www.hipmunk.com/flights-search")
 
 try:
-
-	#tick('* selector')
-	#elements = driver.find_elements_by_css_selector('*')
-	#tock()
+    #tick('* selector')
+    #elements = driver.find_elements_by_css_selector('*')
+    #tock()
 
     #print 'Got %d elements' % len(elements)
 
-	tick('JS enumeration')
-	features = driver.execute_script(open(get_features_js).read())
-	tock()
+    tick('JS enumeration')
+    driver.execute_script(open(underscore_js).read())
+    features = driver.execute_script(open(get_features_js).read())
+    tock()
 
-	print 'Got %d feature vectors' % len(features)
+    print features
+    print 'Got %d feature vectors' % len(features)
 
 finally:
-	time.sleep(15)
-	driver.quit()
+    time.sleep(15)
+    driver.quit()
--- js/get-features.js
+++ js/get-features.js
@@ -1,5 +1,15 @@
 var SHORT_WORD_LEN = 3
-var word_list = filter_word_list(arguments[0])
+var word_list = ["Search", "roflroflrlol", "shit"]; //filter_word_list(arguments[0])
+
+// simplyifying assumption that only these kinds of tags can be targets of click
+// actions
+var CLICKABLE_TAGS = ["A", "BUTTON"];
+
+//typeable tags
+var TYPEABLE_TAGS = ["INPUT"];
+
+var ALL_TAGS = CLICKABLE_TAGS.concat(TYPEABLE_TAGS);
+
 // Compute the edit distance between the two given strings
 // Taken from: http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#JavaScript
 function getEditDistance(a, b){
@@ -34,34 +44,43 @@ function getEditDistance(a, b){
   }
  
   return matrix[b.length][a.length];
-};
+}
 
 
 function filter_word_list(a_word_list) {
     var result = []
-    for (word in a_word_list) { 
+    a_word_list.forEach(function(word) { 
         if (word.length > SHORT_WORD_LEN) {
             result.push(word);
         }
-    }
+    });
 
     return result;
 }
 
+// returns the minimum edit distance between any word in word_set1 and any word in word_set
 function minEditDistanceForWords(word_set1, word_set2) {
+	var minDist = Infinity;
+	word_set1.forEach(function(word1) {
+		var d = minEditDistanceForWord(word1, word_set2);
+		if (d < minDist) {
+            minDist = d;
+		}
+    });
+
+	return minDist;
 }
 
 // returns the minimum edit distance between word and any word in word_set
 function minEditDistanceForWord(word, word_set) {
     // i think this could be optimized with BK trees or somesuch
 	var minDist = Infinity;
-	for (word2 in word_set) {
-		d = getEditDistance(word, word2);
+	word_set.forEach(function(word2) {
+		var d = getEditDistance(word, word2);
 		if (d < minDist) {
-			d = minDist;
+            minDist = d;
 		}
-	}
-
+    });
 	return minDist;
 }
 
@@ -89,8 +108,32 @@ var Features = {
     }
 
     return visible;
-  }
+  },
 
+  // return a list of filtered words from the text of an element
+  getTextWords: function(elem) {
+    return filter_word_list(elem.textContent.split(" "));
+  },
+
+  // produces a list of filtered words in text 
+  // in sibling tags
+  getSiblingTextWords: function(element) {
+      var words = []
+
+      var prev_sibling = element.previousElementSibling;
+      while (prev_sibling != null) {
+          words = words.concat(this.getTextWords(prev_sibling));
+          prev_sibling = prev_sibling.previousElementSibling;
+      }
+
+      var next_sibling = element.nextElementSibling;
+      while (next_sibling != null) {
+          words = words.concat(this.getTextWords(next_sibling));
+          next_sibling = next_sibling.nextElementSibling;
+      }
+
+      return words;
+  },
 }
 
 function elementToFeatureVector(elem) {
@@ -103,7 +146,33 @@ function elementToFeatureVector(elem) {
       // tabIndex
 
     // Features in python_style since it's being shipped off there
-    return [Features.isVisible(elem)? 1:0]
+    return [Features.isVisible(elem)? 1:0, 
+            // check whether its something we can click or type in
+            _.contains(CLICKABLE_TAGS, elem.tagName) ? 1:0,
+            _.contains(TYPEABLE_TAGS, elem.tagName) ? 1:0, 
+
+            minEditDistanceForWord(elem.tagName.toLowerCase(), word_list),
+            minEditDistanceForWords(Features.getTextWords(elem), word_list),
+            minEditDistanceForWords(Features.getSiblingTextWords(elem), word_list),
+
+
+            
+            Features.getTextWords(elem).length,
+            elem.children.length,
+            
+            // Note that left/top might not include scroll offset?
+            // http://ejohn.org/blog/getboundingclientrect-is-awesome/
+            elem.clientTop,
+            elem.clientWidth,
+            elem.clientHeight,
+            // This is another one that might be better treated relative the last
+            // element interacted with, and maybe have -1 special coded to something
+            // for the regression to work better
+            elem.tabIndex,
+            //minEditDistanceForWords(elem.classList, word_list)];
+            elem.textContent, 
+            elem.id,
+            elem.classList];
 }
 
 // produces a list of all Elements in the body of the page that are currently visible
@@ -111,8 +180,10 @@ function getAllElementFeatures(){
   function recGetAllElems(root) {
     var elements = [];
     for(var i=0;i<root.children.length;i++){
-            elements.push(elementToFeatureVector(root.children[i]))
-            elements = elements.concat(recGetAllElems(root.children[i]));
+            var this_child = root.children[i];
+
+            elements.push(elementToFeatureVector(this_child))
+            elements = elements.concat(recGetAllElems(this_child));
     }
     return elements;
   }
