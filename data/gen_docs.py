e707c1f716179c140d3320d15c4c5f2d1e1323ac
Andrew Kovacs <andrewkov@new-host-3.home>
2013-12-26 19:50:25 -0500

    Added function to parse output of gen_docs

--- data/gen_docs.py
+++ data/gen_docs.py
@@ -38,6 +38,30 @@ def get_all_docs(cmds_file):
     rec_gendocs()
     return all_docs
 
+# given a file in the format that this program outputs, parses it into a list of
+# lists of tuples where each tuple contains a text command and a tuple describing the
+# correct action to take for it. Each correct action tuple is a 3-tuple with a
+# type of the command ("type" or "click"), a x-wtid attribute value of the
+# correct target, and a string argument if it is a type command and None if its
+# a click command
+#
+# Each list of these tuples represents a discrete "document" of commands that
+# must be done in the order of the list
+def parse_docs_file(docs_file):
+    docs = []
+    cur_doc = []
+    for l in docs_file:
+        l = l.strip()
+
+        if len(l) == 0:
+            if len(cur_doc) > 0:
+                docs.append(cur_doc)
+                cur_doc = []
+        else:
+            cmd_text, cmd_type, target, arg = l.split("\t")
+            cur_doc.append((cmd_text, (cmd_type, target, arg)))
+    return docs
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train webtalk to generate a parameter vector")
 
@@ -45,7 +69,6 @@ if __name__ == "__main__":
     parser.add_argument("num_docs", type=int, help="A number documents to randomly generate off of the corpus file")
 
     args = parser.parse_args()
-
     for doc in get_all_docs(args.corpus_file)[:args.num_docs]:
         for command in doc:
             txt_cmd, (cmd_type, target, arg) = command
